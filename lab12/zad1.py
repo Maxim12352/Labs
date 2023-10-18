2ae488ae88839f6276e9d5106d20a97c74a88418
Maxim <maximvalux@gmail.com>
2023-10-18 13:33:37 +0300

    fix lab 12

--- lab12/zad1.py
+++ lab12/zad1.py
@@ -1,29 +1,77 @@
-# Створення списку даних про учнів
-students = [
-    {"прізвище": "Іванов", "ім'я": "Петро", "по батькові": "Олександрович", "дата народження": (2005, 3, 15), "стать": "чоловік"},
-    {"прізвище": "Петрова", "ім'я": "Олена", "по батькові": "Ігорівна", "дата народження": (2006, 5, 10), "стать": "жінка"},
-    {"прізвище": "Сидоренко", "ім'я": "Андрій", "по батькові": "Васильович", "дата народження": (2005, 4, 5), "стать": "чоловік"},
-    {"прізвище": "Коваленко", "ім'я": "Марія", "по батькові": "Петрівна", "дата народження": (2006, 3, 20), "стать": "жінка"},
-    {"прізвище": "Павленко", "ім'я": "Олексій", "по батькові": "Іванович", "дата народження": (2005, 3, 25), "стать": "чоловік"},
-    {"прізвище": "Мельник", "ім'я": "Софія", "по батькові": "Михайлівна", "дата народження": (2006, 5, 5), "стать": "жінка"},
-    {"прізвище": "Бойко", "ім'я": "Василь", "по батькові": "Петрович", "дата народження": (2005, 5, 30), "стать": "чоловік"},
-    {"прізвище": "Козлова", "ім'я": "Наталія", "по батькові": "Андріївна", "дата народження": (2006, 3, 10), "стать": "жінка"},
-    {"прізвище": "Гончар", "ім'я": "Дмитро", "по батькові": "Максимович", "дата народження": (2005, 4, 15), "стать": "чоловік"},
-    {"прізвище": "Шевченко", "ім'я": "Катерина", "по батькові": "Олегівна", "дата народження": (2006, 4, 25), "стать": "жінка"},
-    # Додайте інших учнів до списку
-]
+import json
 
-# Запит користувача для введення місяця
-input_month = int(input("Введіть номер місяця (1-12): "))
+# Function to load a JSON file
+def load_json(filename):
+    with open(filename, 'r', encoding="utf-8") as file:
+        data = json.load(file)
+    return data
 
-# Пошук та виведення учнів з днем народження у вказаному місяці
-found_students = False  # Флаг, що показує, чи знайдено учнів
+# Function to save data to a JSON file
+def save_to_json(filename, data):
+    with open(filename, 'w', encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
 
-for student in students:
-    birth_month = student["дата народження"][1]  # другий елемент кортежу - номер місяця
-    if birth_month == input_month:
-        print("{} {} народився у вказаному місяці.".format(student['ім\'я'], student['прізвище']))
-        found_students = True
+# Function to add a new student to the JSON file
+def add_student(filename, student_data):
+    data = load_json(filename)
+    data['students'].append(student_data)
+    save_to_json(filename, data)
 
-if not found_students:
-    print("Учнів, які народилися у вказаному місяці, не знайдено.")
+# Function to find students by birth month
+def find_students_by_month(filename, month):
+    data = load_json(filename)
+    matching_students = []
+
+    for student in data['students']:
+        if student['birth_date']['month'] == month:
+            matching_students.append({
+                "first_name": student["first_name"],
+                "last_name": student['last_name']
+            })
+
+    return matching_students
+
+# Main program
+filename = 'File.json'
+result_filename = 'result.json'
+
+# 1. Display the content of the JSON file
+data = load_json(filename)
+print(json.dumps(data, indent=4))
+
+# 2. Add a new record to the JSON file
+new_student = {
+    "last_name": "New",
+    "first_name": "Student",
+    "middle_name": "MiddleName3",
+    "birth_date": {
+        "year": 2002,
+        "month": 6,
+        "day": 25
+    },
+    "gender": "male"
+}
+add_student(filename, new_student)
+
+# 3. Find data in the JSON file by birth month
+while True:
+    try:
+        month_to_find = int(input("Enter the month number: "))
+        if 1 <= month_to_find <= 12:
+            break
+        else:
+            print("Please enter a valid month number (1-12).")
+    except ValueError:
+        print("Please enter a valid month number (1-12).")
+
+matching_students = find_students_by_month(filename, month_to_find)
+
+if matching_students:
+    print(f"Students born in the month {month_to_find}:")
+    for student in matching_students:
+        print(f"{student['last_name']} {student['first_name']}")
+else:
+    print(f"No students born in the month {month_to_find}.")
+
+# 4. Save the results to another JSON file
+save_to_json(result_filename, {"matching_students": matching_students})
